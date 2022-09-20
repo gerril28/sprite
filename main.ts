ac28a23d41adfbb1129f3c34eacee2fd9c584afe
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-20 09:19:43 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+basic.showLeds(`
+    # . # . .
+    . # . # .
+    # . # . #
+    . # . # .
+    . . # . #
+    `)
+let Num = 0
+basic.showNumber(0)
+let Pic = game.createSprite(2, 2)
